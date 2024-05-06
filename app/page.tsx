c3d47a8e6b8b4a48262b78a324b62e2a83f0378d
matheustheone <matepapava123@gmail.com>
2024-05-06 22:29:08 +0400

    adding

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <main className='h-full w-full'>
+       <div className='flex flex-col gap-20'>
       Mate Papava
-    </div>
+       </div>
+   </main>
   )
+   
+   
 }
 
 export default page
