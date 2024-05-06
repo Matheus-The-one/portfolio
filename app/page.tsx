9aeb6a66f757c7f9a2dabf8a6732689b499d3fdc
matheustheone <matepapava123@gmail.com>
2024-05-06 22:39:39 +0400

    adding public

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from './components/main/Hero'
 
 const page = () => {
   return (
     <main className='h-full w-full'>
        <div className='flex flex-col gap-20'>
-      Mate Papava
+        <Hero />
        </div>
    </main>
   )
