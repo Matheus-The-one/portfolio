7c7eb356defd14f4360c49d8aa3accec45f99432
matheustheone <matepapava123@gmail.com>
2024-05-06 22:47:07 +0400

    adding blackhole

--- app/components/main/Hero.tsx
+++ app/components/main/Hero.tsx
@@ -9,8 +9,9 @@ const Hero = () => {
         muted
         loop
         className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
+        
       >
-        <source src="/blackhole.webm" type="video/webm" />
+        <source src="./blackhole.webm" />
       </video>
       
     </div>
