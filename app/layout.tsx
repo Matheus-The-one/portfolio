acaea53d25912bd8015092e9ef93feff8eec4db9
matheustheone <matepapava123@gmail.com>
2024-05-21 23:59:29 +0400

    isisue with footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <StarsCanvas />
         <Navbar />
         {children}
-        <Footer />
+        
       </body>
     </html>
   )}
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from './components/main/Hero'
 import Skills from './components/main/Skills'
 import Encryption from './components/main/Encryption'
 import Projects from './components/main/Projects'
+import Footer from './components/main/Footer'
 
 const page = () => {
   return (
@@ -12,6 +13,7 @@ const page = () => {
         <Skills />
         <Encryption />
         <Projects />
+        <Footer />
        </div>
    </main>
   )
