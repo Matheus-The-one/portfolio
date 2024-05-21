db5c8a3c062eef35ccbd66d9b430c3d0821867ed
matheustheone <matepapava123@gmail.com>
2024-05-21 23:43:33 +0400

    motin desgine

--- app/components/main/Encryption.tsx
+++ app/components/main/Encryption.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 import { motion } from "framer-motion";
-import { slideInFromTop } from "@/utils/motion";
+import { slideInFromTop } from "@/app/utils/motion";
 import Image from "next/image";
 
 const Encryption = () => {
