a5332bf6050f61d348b70c747e28e3502e7367a3
matheustheone <matepapava123@gmail.com>
2024-05-23 01:47:28 +0400

    adding skills section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Hero from './components/main/Hero'
-import Skills from './components/main/Skills'
+
 import Encryption from './components/main/Encryption'
 import Projects from './components/main/Projects'
 import Footer from './components/main/Footer'
+
+import Grid from './components/main/Grid'
 
 const page = () => {
   return (
     <main className='h-full w-full'>
        <div className='flex flex-col h-[850px] gap-20'>
         <Hero />
-        <Skills />
+        <Grid/>
         <Encryption />
         <Projects />
         <Footer />
