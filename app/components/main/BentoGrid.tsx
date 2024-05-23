9869afaac776265b92b5c4a261017d5538bb185f
matheustheone <matepapava123@gmail.com>
2024-05-23 22:29:50 +0400

    adding some tech skills

--- app/components/main/BentoGrid.tsx
+++ app/components/main/BentoGrid.tsx
@@ -53,8 +53,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "Express", "Typescript","Javascript","SQL","C/C++"];
+  const rightLists = ["Python", "NextJS", "GraphQL","Tailwind CSS","ThreeJS"];
 
   const [copied, setCopied] = useState(false);
 
