5c5e59ab6360632db585d72939fb6a0cebd0be3e
matheustheone <matepapava123@gmail.com>
2024-05-27 23:16:23 +0400

    adding responsive design

--- app/components/main/Grid.tsx
+++ app/components/main/Grid.tsx
@@ -4,7 +4,7 @@ import { BentoGrid, BentoGridItem } from "./BentoGrid";
 const Grid = () => {
   return (
     <section id="about">
-      <BentoGrid className="w-[1350px] mx-12 py-20 text-white">
+      <BentoGrid className="w-auto mx-12 py-20 text-white">
         {gridItems.map((item, i) => (
           <BentoGridItem
             id={item.id}
