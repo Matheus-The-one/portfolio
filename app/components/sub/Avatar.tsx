4eb0769e93ab822b2806417044edf21314c44fda
matheustheone <matepapava123@gmail.com>
2024-05-27 23:53:01 +0400

    ....

--- app/components/sub/Avatar.tsx
+++ app/components/sub/Avatar.tsx
@@ -8,15 +8,15 @@ import { Group } from 'three';
 export const Avatar: React.FC = () => {
     return (
         <Canvas style={{ height: '200px', width: '55%' }}>
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[0, 10, 5]} intensity={1} />
+            <ambientLight intensity={0.5} color="#ffffff" />
+            <directionalLight position={[0, 10, 5]} intensity={1} color="#ffffff" />
             <AvatarModel />
         </Canvas>
     );
 };
 
 export const AvatarModel: React.FC = () => {
-    const myModel = useLoader(GLTFLoader, '/big_boy.glb');
+    const myModel = useLoader(GLTFLoader, '/avatarmale.glb');
     const modelRef = useRef<Group>(null);
 
     useFrame((_state, delta) => {
