1370c1ff3f5e175719c465e0db2a94fe6e322a40
matheustheone <matepapava123@gmail.com>
2024-05-27 23:56:59 +0400

    ...

--- app/components/sub/Avatar.tsx
+++ app/components/sub/Avatar.tsx
@@ -2,14 +2,12 @@
 
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { useRef } from 'react';
+import { Mesh } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { Group } from 'three';
 
 export const Avatar: React.FC = () => {
     return (
-        <Canvas style={{ height: '200px', width: '55%' }}>
-            <ambientLight intensity={0.5} color="#ffffff" />
-            <directionalLight position={[0, 10, 5]} intensity={1} color="#ffffff" />
+        <Canvas style={{ height: '200px', width: '65%' }}>
             <AvatarModel />
         </Canvas>
     );
@@ -17,14 +15,20 @@ export const Avatar: React.FC = () => {
 
 export const AvatarModel: React.FC = () => {
     const myModel = useLoader(GLTFLoader, '/avatarmale.glb');
-    const modelRef = useRef<Group>(null);
+    const modelRef = useRef<Mesh>(null);
 
     useFrame((_state, delta) => {
         if (modelRef.current) {
             modelRef.current.rotation.y += delta / 2;
+            
         }
     });
 
-    return <primitive object={myModel.scene} ref={modelRef} />;
+    return (
+        <>
+              <ambientLight intensity={0.3} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+            <primitive object={myModel.scene} ref={modelRef} />
+        </>
+    );
 };
-
