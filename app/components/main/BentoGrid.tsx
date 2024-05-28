b7071b1cffa5bfe4d34bfabc2ff72b895de4bad1
matheustheone <matepapava123@gmail.com>
2024-05-28 22:58:21 +0400

    editing skills

--- app/components/main/BentoGrid.tsx
+++ app/components/main/BentoGrid.tsx
@@ -147,7 +147,7 @@ export const BentoGridItem = ({
                     className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
                     lg:opacity-100 rounded-lg text-center bg-[#10132E]"
                   >
-                    {skill.Image}
+                    <img src={skill.Image} alt="" className="w-[30px] h-[30px]" />
                   </span>
                 ))}
                 <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
