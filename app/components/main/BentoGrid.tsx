87c32235c3ffd18a2e70382b4d01b9678c7605ea
matheustheone <matepapava123@gmail.com>
2024-05-28 23:26:37 +0400

    editing skills section

--- app/components/main/BentoGrid.tsx
+++ app/components/main/BentoGrid.tsx
@@ -138,27 +138,27 @@ export const BentoGridItem = ({
 
           {/* Tech stack list div */}
           {id === 3 && (
-            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="">
               {/* tech stack lists */}
-              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+              <div className="grid gap-4 grid-cols-3 grid-rows-3">
                 {Frontend_skill.map((skill) => (
                   <span
                     key={skill.Image}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    // className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    // lg:opacity-100 rounded-lg text-center bg-[#10132E]"
                   >
                     <img src={skill.Image} alt="" className="w-[30px] h-[30px]" />
                   </span>
                 ))}
-                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                
               </div>
-              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              <div className=" grid gap-4 grid-cols-3 grid-rows-3">
+                
                 {Backend_skill.map((skill) => (
                   <span
                     key={skill.Image}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    // className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    // lg:opacity-100 rounded-lg text-center bg-[#10132E]"
                   >
                     <img src={skill.Image} className="w-[30px] h-[30px]"></img>
                   </span>
