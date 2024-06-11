b96b80bcf7c49d407a2d632ea7e2db15dc299d35
matheustheone <matepapava123@gmail.com>
2024-06-11 23:29:36 +0400

    editing the blog

--- app/components/main/BentoGrid.tsx
+++ app/components/main/BentoGrid.tsx
@@ -183,7 +183,7 @@ export const BentoGridItem = ({
               </div>
 
               <MagicButton
-                title={copied ? "Email is Copied!" : "Copy my email address"}
+                title={copied ? "Opened in new Tab" : "Visit My Blog"}
                 icon={<IoCopyOutline />}
                 position="left"
                 handleClick={handleCopy}
