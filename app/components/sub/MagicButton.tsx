1da49c96c9e013ccee76f8f778fa997ed063dd41
matheustheone <matepapava123@gmail.com>
2024-06-14 23:38:25 +0400

    adding button elements

--- app/components/sub/MagicButton.tsx
+++ app/components/sub/MagicButton.tsx
@@ -9,24 +9,25 @@ import React from "react";
  *  remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
  */
 
-
 const MagicButton = ({
   title,
   icon,
   position,
-  handleClick,
+  link,
   otherClasses,
 }: {
   title: string;
   icon: React.ReactNode;
   position: string;
-  handleClick?: () => void;
+  link: string;
   otherClasses?: string;
 }) => {
   return (
-    <button
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
       className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
-      onClick={handleClick}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 
@@ -39,7 +40,7 @@ const MagicButton = ({
         {title}
         {position === "right" && icon}
       </span>
-    </button>
+    </a>
   );
 };
 
