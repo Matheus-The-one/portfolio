da7b02807f3fb284f4796053d300c33600319655
matheustheone <matepapava123@gmail.com>
2024-06-16 20:05:19 +0400

    editing data for the startup section

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -261,7 +261,7 @@ export const Socials = [
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
       titleClassName: "justify-end",
-      img: "",
+      img: "/",
       spareImg: "",
     },
     {
@@ -271,7 +271,7 @@ export const Socials = [
       className: "lg:col-span-2 md:col-span-3 md:row-span-2",
       imgClassName: "",
       titleClassName: "justify-start",
-      img: "",
+      img: "/",
       spareImg: "",
     },
     {
@@ -281,7 +281,7 @@ export const Socials = [
       className: "lg:col-span-2 md:col-span-3 md:row-span-2",
       imgClassName: "",
       titleClassName: "justify-center",
-      img: "",
+      img: "/",
       spareImg: "",
     },
     {
@@ -291,7 +291,7 @@ export const Socials = [
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
       titleClassName: "justify-start",
-      img: "",
+      img: "/",
       spareImg: "",
     },
   
@@ -300,10 +300,17 @@ export const Socials = [
       title: "Currently building a Gnerative AI solution With my start up team",
       description: "Besides Work",
       className: "md:col-span-3 md:row-span-2",
-      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      imgClassName: "absolute right-5 bottom-40 md:w-96 w-90 h-90 sm:w-20",
       titleClassName: "justify-center md:justify-start lg:justify-center",
-      img: "",
+      img: "/stamoai.png",
       spareImg: "",
+      button: {
+        text: "Visit Stamo AI",
+        url: "https://stamoai.vercel.app/",
+        className: "mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded",
+        target: "_blank",
+        rel: "noopener noreferrer"
+      }
       
     },
     {
@@ -313,7 +320,7 @@ export const Socials = [
       className: "lg:col-span-2 md:col-span-3 md:row-span-1 cursor-pointer",
       imgClassName: "",
       titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-      img: "",
+      img: "/",
       spareImg: "",
     },
   ];
