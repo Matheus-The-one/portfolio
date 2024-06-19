441ce38b98970a1ea3629de2e9a34665ce9bc64b
matheustheone <matepapava123@gmail.com>
2024-06-20 00:23:02 +0400

    editing th main page

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import Encryption from './components/main/Encryption'
 import Projects from './components/main/Projects'
 import Footer from './components/main/Footer'
 
-import Grid from './components/main/Grid'
+
 
 const page = () => {
   return (
     <main className='h-full w-full'>
        <div className=' flex flex-col h-[850px] gap-20'>
         <Hero />
-        <Grid />
+        
         <Encryption />
         <Projects />
         <Footer />
