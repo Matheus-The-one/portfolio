373158159d1d7f67a10f01bad225bf3cdcd28f94
matheustheone <matepapava123@gmail.com>
2024-08-13 01:12:41 +0400

    ..

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,23 +18,16 @@ module.exports = {
     "next/core-web-vitals",
   ],
   rules: {
-    "prettier/prettier": ["warn", { semi: true }], // Change to "warn" to avoid build failure on formatting issues
-    "react/react-in-jsx-scope": "off", 
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        components: ["Link"],
-        specialLink: ["hrefLeft", "hrefRight"],
-        aspects: ["invalidHref", "preferButton"],
-      },
-    ],
+    "prettier/prettier": ["warn", { semi: true }],
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/anchor-is-valid": "off", // Disable anchor href validation
     "react/prop-types": "off",
     "no-unused-vars": "off",
     "react/no-unescaped-entities": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-explicit-any": ["warn"], // Keep as "warn" to allow usage of "any" type without build failure
-    "no-console": "warn", // Optionally add this rule to warn about console usage without failing the build
+    "@typescript-eslint/no-explicit-any": "off", // Disable the rule for no explicit any
+    "no-console": "off", // Disable console.log warning
   },
 };
